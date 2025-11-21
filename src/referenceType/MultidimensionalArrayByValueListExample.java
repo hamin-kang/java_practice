6471cd87672ed208e5edac62f61487e62b249f07
hamin <kyden5386@gmail.com>
2025-11-21 23:49:43 +0900

    1121

---
+++ src/referenceType/MultidimensionalArrayByValueListExample.java
@@ -0,0 +1,11 @@
+package referenceType;
+
+public class MultidimensionalArrayByValueListExample {
+    public static void main(String[] args) {
+        // 2차원 배열 생성
+        int[][] scores = {
+                {80, 90, 96},
+                {76, 88},
+        };
+    }
+}
