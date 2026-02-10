a2b9b55f17e6e6cb99e0a3ddd065c66fc877a633
hamin <kyden5386@gmail.com>
2026-02-10 23:06:59 +0900

    new

--- src/referenceType/MultidimensionalArrayByNewExample.java
+++ src/referenceType/MultidimensionalArrayByNewExample.java
@@ -1,4 +1,10 @@
 package referenceType;
 
 public class MultidimensionalArrayByNewExample {
+    public static void main(String[] args) {
+        int[][] mathScores = new int[2][3];
+        for (int i = 0; i < mathScores.length; i++) {
+
+        }
+    }
 }
